e1939d94037fef25e74ee2b977ef8a95c4c5d760
Yusaku-KASAI <kasai.yusaku.work@gmail.com>
2024-12-10 18:58:25 +0900

    setup dir

--- vue/src/router.js
+++ vue/src/router.js
@@ -1,6 +1,14 @@
 import { createWebHistory, createRouter } from "vue-router";
 
-const routes = [];
+import Home from "@pages/Home.vue";
+import Test from "@pages/Test.vue";
+import About from "@pages/About.vue";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/test", component: Test },
+  { path: "/about", component: About },
+];
 
 export const router = createRouter({
   history: createWebHistory(),
